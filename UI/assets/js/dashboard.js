06d91e17a53e295fe2152b1debaad4ed0f710419
Chinedu <oriechinedu@gmail.com>
2019-01-22 08:49:48 +0100

    [Chore #163349108] Implement dashboard layout

--- UI/assets/js/dashboard.js
+++ UI/assets/js/dashboard.js
@@ -1,23 +1,9 @@
 
-let click = 1;
 const dashNavToggle = document.getElementById('dash__nav__toggle');
-const navLateral = document.getElementById("sidebar__menu");
-const navbar = document.getElementById("nabvar");
-const main = document.getElementById("main");
+const sidebar = document.getElementById("sidebar__menu");
 dashNavToggle.addEventListener('click', () =>{
-	if(click ==  1){
-    navLateral.style.display    = "none";
-    navbar.style.width          = "100%";
-    navbar.style.left           = "0";
-    main.style.width            = "100%";
-    click = click + 1;
-   } 
-   else {
-      navLateral.style.display  = "block";
-      navbar.style.width        = "82%";
-      navbar.style.left         = "18%";
-      main.style.width          = "82%";
-      click = 1;
-   }
+
+  sidebar.classList.toggle('show-sidebar');
+  
 
 });
