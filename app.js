e9c3d884845ca4beaf6d7d4a41eabec97617b7ed
Orie Chinedu Emmanuel <31114577+oriechinedu@users.noreply.github.com>
2019-01-29 11:37:38 +0100

    Revert "Admin create political parties 163524062"

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import debug from 'debug';
 import morgan from 'morgan';
-import expressValidator from 'express-validator';
-import partyRouter from './src/routes/PartyRoutes';
 
 const app = express();
 const debugg = debug('app');
-const port = process.env.PORT || 3000;
-app.use(expressValidator());
+const port = process.env.PORT || 5000;
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.use('/api/v1/parties', partyRouter);
 app.get('/', (req, res) => {
   res.send('welcome to Politico');
 });
@@ -22,5 +16,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   debugg(`App started at port ${port}`);
 });
-
-export default app;
--- src/routes/PartyRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import PartyController from '../controllers/PartyController';
-import PartyValidator from '../middlewares/PartyValidator';
-
-const partyRouter = express.Router();
-
-partyRouter.post('/', PartyValidator.createPartyValidator, PartyController.create);
-
-export default partyRouter;
