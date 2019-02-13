29fad87d1d070f9b1ecbf0baeb9b9ba0dc0e02f2
Chinedu <oriechinedu@gmail.com>
2019-02-13 08:49:12 +0100

    feat(vote): implement view offices and view candidates integration

---
+++ UI/assets/js/politicalOffices.js
@@ -0,0 +1,52 @@
+const tableBody = document.getElementById('table-body');
+const pagination = document.getElementsByClassName('pagination-container')[0];
+
+pagination.style.display = 'none';
+const url = 'https://oriechinedu-politico.herokuapp.com/api/v1/offices';
+const options = {
+  method: 'GET',
+  headers: new Headers({ 'Content-Type': 'application/json' }),
+};
+const request = new Request(url, options);
+fetch(request)
+  .then(res => res.json())
+  .then((res) => {
+    if (res.status === 200) {
+      const { data } = res;
+      if (data.length) {
+        let row;
+        data.forEach((office, index) => {
+          row = `<tr>
+                    <td>${index + 1}</td>
+                    <td class="text-uppercase">${office.type}</td>
+                    <td>${office.name}</td>
+                    <td>
+                        
+                          <button class="btn btn-info" data-officeId="${office.id}" onclick="proceedToVote(event)">
+                             Proceed to Vote
+                          </button>
+                        
+                    </td>
+                </tr>`;
+          tableBody.insertAdjacentHTML('afterbegin', row);
+        });
+        if (data.length >= 15) {
+          pagination.style.display = 'flex';
+        }
+      } else {
+        tableBody.insertAdjacentHTML('afterbegin', '<p>No office at the moment</p>');
+        pagination.style.display = 'none';
+      }
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const proceedToVote = (e) => {
+  e.preventDefault();
+  const officeId = e.target.getAttribute('data-officeId');
+  localStorage.setItem('officeId', officeId);
+  window.location.replace('candidates.html');
+};
