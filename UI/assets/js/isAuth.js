abd6584cd719cd919092f4df80d96352e85917be
Chinedu <oriechinedu@gmail.com>
2019-02-15 20:43:48 +0100

    Logout user if token expires

--- UI/assets/js/isAuth.js
+++ UI/assets/js/isAuth.js
@@ -1,13 +1,28 @@
 const accessToken = localStorage.getItem('token');
-
 const isAuthenticated = (tkn) => {
+  if (!tkn) window.location = 'login.html';
   if (tkn) {
-    const tokenComponent = tkn.split('.');
-    if (tokenComponent.length === 3) {
-      return true;
-    }
+    const body = { token: tkn };
+    const url = 'https://oriechinedu-politico.herokuapp.com/api/v1/token/validate';
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+    const options = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body),
+    };
+    const request = new Request(url, options);
+    fetch(request)
+      .then(response => response.json())
+      .then((response) => {
+        if (response.status === 200) {
+          return true;
+        }
+        window.location = 'login.html';
+      })
+      .catch((err) => { if (err) return false; });
   }
-  window.location = 'login.html';
 };
 
 isAuthenticated(accessToken);
